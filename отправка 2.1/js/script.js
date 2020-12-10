9c6cdcf1b9f093a50be55227f4a5b0382606e25f
Alexandr-kiselev <62692939+Alexandr-kiselev@users.noreply.github.com>
2020-12-10 16:19:38 +0500

    Add files via upload

---
+++ отправка 2.1/js/script.js
@@ -0,0 +1,34 @@
+// PopUp
+$(document).ready(function(){
+    PopUpHide();
+});
+function PopUpShow(){
+    $("#popup1").show();
+}
+function PopUpHide(){
+    $("#popup1").hide();
+}
+
+
+
+//Отправка на сервер
+$(document).ready(function() {
+
+	//E-mail Ajax Send
+	$("form").submit(function() { //Change
+		var th = $(this);
+		$.ajax({
+			type: "POST",
+			url: "mail.php", //Change
+			data: th.serialize()
+		}).done(function() {
+			alert("Спасибо за обращение");
+			setTimeout(function() {
+				// Done Functions
+				th.trigger("reset");
+			}, 1000);
+		});
+		return false;
+	});
+
+});
