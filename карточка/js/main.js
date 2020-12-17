d5cf8af2fdfff385152041628469bfa70f9bcf04
Alexandr-kiselev <62692939+Alexandr-kiselev@users.noreply.github.com>
2020-12-18 01:20:03 +0500

    Add files via upload

---
+++ карточка/js/main.js
@@ -0,0 +1,49 @@
+// $(function () {
+//     $('.push').on('click', function () {
+//         $('.front').css({ 'transform': 'rotateY(180deg)' })
+//         $('.back').css({ 'transform': 'rotateY(360deg)' })
+//     });
+
+//     $('.push_back').on('click', function () {
+//         $('.front').css({ 'transform': 'rotateY(0deg)' })
+//         $('.back').css({ 'transform': 'rotateY(180deg)' })
+//     });
+
+// });
+
+
+    
+    // $('.push').on('click', function(){
+    //     $(this).css({ 'transform': 'rotateY(180deg)' })
+    // })
+    
+//   $(function (ro,go) {
+//     $('.push').on('click', function () {
+//         $('.front').css({ 'transform': 'rotateY(180deg)' })
+//         $('.back').css({ 'transform': 'rotateY(360deg)' })
+//     });
+
+//     $('.push_back').on('click', function () {
+//         $('.front').css({ 'transform': 'rotateY(0deg)' })
+//         $('.back').css({ 'transform': 'rotateY(180deg)' })
+//     });
+
+// });
+
+// -----------------------------------------------
+// let push = document.querySelectorAll('.push'),
+//     front = document.querySelectorAll('.front'),
+//     back = document.querySelectorAll('.back');
+//       for (var b in push)
+//       push[b].onclick = function(){
+//         this.parentNode.style.background = 'red';
+//       };
+
+// function bac(){
+//       for (var r in back)
+//       back[r]
+// }
+// -------------------------------------
+
+let button = $(this).parent('.push')
+button.css()
